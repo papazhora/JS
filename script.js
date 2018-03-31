d012a909972b86a2607dfede4e5e5e3b19ea310b
papazhora <4156464@gmail.com>
2018-03-31 16:22:35 +0300

    js

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-// alert ('gggggggggggggggg');
+alert ('gggggggggggggggg');
 
+var name=10;
+alert(name);
+
+var a=5+6;
+alert('a'+" "+a);
+
+var str = 'ggggggggggggg';
+alert('str'+' '+str);
+
+var massage='Hello';
+massage='World';
+alert(massage);
+
+var ddd_$;
+ddd_$=50;
+alert(ddd_$);
+alert('ddd_$');
+
+var massege1;
+massege1='dddddddddddddd';
+
+massage2=60;
+
+massege3="rrrrrrrrrrrrrrrr";
+
+var massege4="eeeeeeeeeeeeee";
+
+alert( massege1);
+
+$$__=23;
+
+__=30;
+
+var $_____$=55;
+
+alert($_____$);
+alert($$__);
+alert(__);
